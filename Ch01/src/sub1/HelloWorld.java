8b0616406097ee7f7e1fc5f11a960b8cc6d24ab2
rlaalsdn0821 <kimminwoo0821@naver.com>
2021-05-18 12:53:09 +0900

    2021/05/17 java Ch01 수업예제 추가

--- Ch01/src/sub1/HelloWorld.java
+++ Ch01/src/sub1/HelloWorld.java
@@ -3,10 +3,11 @@ package sub1;
 /*
 날짜: 2021/05/17
 이름: 김민우
-내용: HelloWorld 출력
+내용: HelloWorld 출력하기
 */
 
 public class HelloWorld {
+	
 	public static void main(String[] args) {
 		System.out.println("Hello world~");
 	}
